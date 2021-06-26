b5818b0180b1d54ff48a833c7eba4b11d62f2220
adepoju2006 <adepojuayomide2006@gmail.com>
2021-06-26 14:50:57 +0100

    first commit

---
+++ captain.js
@@ -0,0 +1,16 @@
+// var captain =prompt("Enter science captain name");
+
+// if(captain=="kabirat"){
+//     alert("Happy Birthday to science captain"+" "+captain);
+// } else{
+//     alert("Incorrect captain name")
+// };
+
+
+var talk=prompt("Enter Talkative Name");
+
+if (talk == "Jamiu Olalere"){
+    alert("Jamiu Olalere The Talkative Man");
+} else{
+    alert("Incorrect Talkative Name");
+}
